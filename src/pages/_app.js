8faf04472c51e042de4cc2a62a3d768b477c135c
Markus <markus.sibbe@t-online.de>
2024-03-20 11:54:50 +0100

    add styled-components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import GlobalStyle from "../styles/globalStyles";
+import GlobalStyle from "../styles/styles.js";
 import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
